10c16560fd36bebb45ffaa64d0989e8ab657580f
Stephen Emblem <stephenaemblem@gmail.com>
2020-08-10 20:03:28 -0400

    Improve Tackle damage animation
    Now its not based on a certain length of time, which made all damage amounts
    take the same amount of time, but based on a interval which take away 1 health
    every 50 milliseconds

--- src/moves/Tackle.js
+++ src/moves/Tackle.js
@@ -3,35 +3,25 @@ export default class Tackle {
   constructor(p){
     this.p = p;
     this.damageAnimationStart = 0;
-    this.damageAnimationStartHealth = 0;
-    this.damageAnimationLength = 1000;
+    this.damageStartHealth = 0;
+    this.damageInterval = 50;
     this.finished = false;
   }
   start(opponentPokemon){
     //opponent is the pokemon that is being attacked
     // can be the player pokemon when the ai pokemon is using this move
     this.damageAnimationStart = this.p.millis();
-    this.damageAnimationStartHealth = opponentPokemon.health;
+    this.damageStartHealth = opponentPokemon.health;
   }
   preform(attackingPokemon, opponentPokemon){
-    opponentPokemon.health = this.p.max(this.damageMap(attackingPokemon), 0);
+    if(this.p.millis() > this.damageAnimationStart + this.damageInterval){
+      this.damageAnimationStart = this.p.millis();
+      opponentPokemon.health--;
+    }
 
-    if(this.isDamageAnimationOver()){
-      opponentPokemon.health = this.damageAnimationStartHealth - attackingPokemon.attack;
+    if(opponentPokemon.health == this.damageStartHealth - attackingPokemon.attack){
       return true;
     }
     return false;
-  }
-  isDamageAnimationOver(entity){
-    return this.p.millis() > this.damageAnimationStart + this.damageAnimationLength;
-  }
-  damageMap(attackingPokemon){
-    // this is not the right way to do this
-    // small damage amount will take just as long as large damage amounts
-    return this.p.int(this.p.map(this.p.millis(),
-                                 this.damageAnimationStart,
-                                 this.damageAnimationStart + this.damageAnimationLength,
-                                 this.damageAnimationStartHealth,
-                                 this.damageAnimationStartHealth - attackingPokemon.attack));
   }
 }
